f8d75bd292111ae506c301e33e86a47b8e9156c5
danosella <danosella96@gmail.com>
2022-11-27 10:50:10 +0700

    update delete inner join

--- src/main/java/com/hql/HQLExample.java
+++ src/main/java/com/hql/HQLExample.java
@@ -1,11 +1,13 @@
 package com.hql;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.tutor.Student;
@@ -42,6 +44,35 @@ public class HQLExample {
 			System.out.println(student.getName()+" : "+student.getCerti().getCourse());
 		}
 		
+		System.out.println("________________________________________");
+		
+		Transaction tx = s.beginTransaction();
+		
+//		Query q2 = s.createQuery("delete from Student s where s.city=:c");
+		//delete query
+//		q2.setParameter("c", "Palu");		
+//		int r = q2.executeUpdate();
+//		System.out.println("Deleted :");
+//		System.out.println(r);
+		
+		//update query
+		Query q2 = s.createQuery("update Student set city=:c where name=:n");
+		q2.setParameter("c", "Malang");
+		q2.setParameter("n", "Irfan");
+		int r = q2.executeUpdate();
+		System.out.println(r + " objects updated");
+		
+		tx.commit();
+		
+		//bagaimana mengeksekusi join query
+		Query q3 = 
+				s.createQuery("select q.question, q.questionId, a.answer from Question as q INNER JOIN q.answers as a");
+	 	
+		List<Object[]> list3 = q3.getResultList();
+	 	
+	 	for (Object[] arr : list3) {
+	 		System.out.println(Arrays.toString(arr));
+	 	}
 		
 		s.close();
 		factory.close();
